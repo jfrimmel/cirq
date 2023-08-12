cd7ff28ec1a85fb88b7f9256e02b079afe2412d2
ci-build <ci-build@cirq>
2023-08-12 10:52:36 +0000

    [skip ci] Updates

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+/// <reference lib="esnext" />
+/// <reference lib="dom" />
+'use strict';
+window.onload = function () {
+    // if supported in the current browser (and mode), then register the service
+    // worker for the PWA. A browser might not support this for various reasons,
+    // e.g. it simply does not support PWAs or it runs in private mode or the
+    // connection to the site is untrusted/unencrypted.
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('worker.js');
+    }
+};
