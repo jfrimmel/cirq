c39dcce536061c54e0595c799630949406899294
Julian Frimmel <julian@fri-me.de>
2023-08-18 21:45:22 +0200

    Render a full-screen X onto a canvas
    This commit uses `web-sys` to insert a canvas into the `<body>` and is
    using the 2D rendering context to render a full-screen X onto that new
    canvas. This way a simply rendering can be shown.
    
    Note, that this commit does not use `winit` but does the rendering-
    "loop" all by hand (currently only when resizing). In the future, one
    may want to migrate to winit for easier cross-platform application or
    generally easier development.

--- src/wasm/lib.rs
+++ src/wasm/lib.rs
@@ -1,11 +1,49 @@
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[wasm_bindgen(start)]
+pub fn main() {
+    let window = web_sys::window().expect("Failed to get `window`");
+    let document = window.document().expect("Failed to get `document`");
+
+    let canvas = document
+        .create_element("canvas")
+        .expect("<canvas> is a supported name")
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .expect("`rendering-surface` must be a <canvas>");
+    document.body().unwrap().append_child(&canvas).unwrap();
+
+    let canvas = Rc::new(canvas);
+    let window = Rc::new(window);
+
+    let on_resize: Closure<dyn Fn()> = Closure::new({
+        let window = Rc::clone(&window);
+        let canvas = Rc::clone(&canvas);
+        move || draw(&window, &canvas)
+    });
+    window.set_onresize(Some(on_resize.into_js_value().unchecked_ref()));
+
+    draw(&window, &canvas);
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, world!");
+fn draw(window: &web_sys::Window, canvas: &web_sys::HtmlCanvasElement) {
+    let width = window.inner_width().unwrap().as_f64().unwrap().round() as u32;
+    let height = window.inner_height().unwrap().as_f64().unwrap().round() as u32;
+
+    canvas.set_width(width);
+    canvas.set_height(height);
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.begin_path();
+    context.move_to(0.0, 0.0);
+    context.line_to(width as f64, height as f64);
+    context.move_to(0.0, height as f64);
+    context.line_to(width as f64, 0.0);
+    context.stroke();
 }
